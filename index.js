3551ac25c07ddb18fad76399e7d71b6f5fa77133
Yavuz Ergun <posta.yavuzergun@gmail.com>
2022-04-03 19:07:28 +0300

    1

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const submitButton= document.querySelector('.onayla');
 const mainContainer= document.querySelector('.container-main');
 const ratingContainer= document.querySelector('.container-rating');
 const ratings= document.querySelector('.nums');
+
 // const thankUContainer= document.querySelector('container-thankU');
 
 ratings.addEventListener('click',ratingYaz);
-let rating = 0;
+let rating = undefined;
 
 function ratingYaz(e){
     
@@ -18,6 +19,7 @@ submitButton.addEventListener('click', thankU);
 
 function thankU(e){
     e.preventDefault();
+    if (rating){ 
     const thankU= document.createElement('div');
     thankU.className='container-thankU';
     thankU.innerHTML= `<img src="images/illustration-thank-you.svg" alt="">
@@ -32,4 +34,15 @@ function thankU(e){
       don’t hesitate to get in touch!
       </p>`;
     mainContainer.replaceChild(thankU, ratingContainer);
+  } else{
+  const warn= document.createElement('div');
+  warn.className='container-warn';
+  warn.innerHTML= `<img src="images/circle-exclamation-solid.svg" alt="" <i class="fa-solid fa-circle-exclamation"></i>>
+  
+    <h1>Please select any rating!</h1>
+    
+  `;
+  mainContainer.replaceChild(warn, ratingContainer);
+
+  }
 }
